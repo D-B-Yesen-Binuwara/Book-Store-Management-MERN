b5765d0e68907a4600801c9ef6bae1471fdd8c3d
D-B-Yesen-Binuwara <yesenbinuwara2002@gmail.com>
2025-12-12 01:14:20 +0530

    Show-book & create-book pgs

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 import React from 'react'
-import {Routes, route} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
 import ShowBook from './pages/ShowBook';
 import CreateBooks from './pages/CreateBooks';
@@ -44,11 +44,11 @@ import EditBook from './pages/EditBook';
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={Home} />
+      <Route path='/' element={<Home />} />
       <Route path='/books/create' element={<CreateBooks />} />
-      <Route path='/book/delete/:id' element={<DeleteBook />} />
-      <Route path='/book/details/:id' element={<ShowBook />} />
-      <Route path='/book/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
     </Routes>
   )
 }
